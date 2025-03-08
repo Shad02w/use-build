7ddd49fd6d134ed0d3552fdb7b813a0828b5db17
Shad02w <alvistse123@gmail.com>
2025-03-08 06:17:00 -0500

    chore(ci): try to fix paths in tsup config

--- core/tsup.config.ts
+++ core/tsup.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "tsup"
-import { fileURLToPath } from "node:url"
-import path from "node:path"
-
-const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-    entry: [path.join(dirname, "./src/vite.ts"), path.join(dirname, "./src/rsbuildv2/index.ts")],
+    entry: ["./src/vite.ts", "./src/rsbuildv2/index.ts"],
     skipNodeModulesBundle: true,
     external: ["vite", "@rsbuild/core", "@rspack/core", "express"],
     dts: true,
-    outDir: path.join(dirname, "./dist"),
+    outDir: "./dist",
+    clean: true,
     format: ["cjs", "esm"]
 })
