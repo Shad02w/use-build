f6c5102c37fc245497fec4010a05e08bf7d73329
Shad02w <alvistse123@gmail.com>
2025-09-02 01:03:15 -0400

    feat: update and test support on vite 7 and rsbuild 1.5

---
+++ test/vite-7-react/src/util/uuid.ts
@@ -0,0 +1,3 @@
+"use build"
+
+export const uuid = crypto.randomUUID() + "123459123"
--- test/vite-7-react/vite.config.ts
+++ test/vite-7-react/vite.config.ts
@@ -5,6 +5,5 @@ import { UseBuildPlugin } from "use-build/vite"
 // https://vite.dev/config/
 export default defineConfig({
     environments: {},
-    // @ts-ignore -- vite plugin api type verison mismatch
     plugins: [react(), UseBuildPlugin()]
 })
