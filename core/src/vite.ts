fd446d303448e97c8a447e2a355e95e518ed95f1
Shad02w <alvistse123@gmail.com>
2026-04-14 03:57:45 -0400

    refactor(vite): use filter before go into transform

--- core/src/vite.ts
+++ core/src/vite.ts
@@ -4,10 +4,6 @@ import type { ModuleRunner } from "vite/module-runner"
 
 const name = "use-build"
 
-const cache = new Map<string, any>()
-
-const ids = new Set<string>()
-
 export function UseBuildPlugin(): Plugin {
     const logger = createLogger("info", { prefix: `[${name}]` })
     let userConfig: UserConfig
@@ -39,41 +35,45 @@ export function UseBuildPlugin(): Plugin {
                 plugins: []
             })
         },
-        async transform(code, id) {
-            if (!(await isBuildTimeFile(id, code))) return
-
-            cache.set(id, code)
-            ids.add(id)
-
-            const start = Date.now()
-            logger.info("\n")
-            logger.info(`Running build time script: ${id}`, { timestamp: true })
-
-            const ssrEnv = buildServer.environments.ssr
-            const runner = (ssrEnv as any)["runner"] as ModuleRunner
-
-            try {
-                const modules = await runner.import(id)
-                logger.info(`Done in ${Date.now() - start}ms`, { timestamp: true })
-
-                const currentModule = buildServer.moduleGraph.getModuleById(id)
-
-                Array.from(currentModule?.importedModules ?? []).forEach(m => {
-                    if (m.id) {
-                        this.addWatchFile(m.id)
-                    }
-                })
-
-                return serializeModules(modules, id)
-            } catch (e) {
-                if (e instanceof Error) {
-                    throw new Error(`Failed to run build time script: ${id} \n${e.message}\n${e.stack}`)
+        transform: {
+            filter: {
+                code: {
+                    include: ["use build"],
+                    ignoreCase: true
                 }
-                throw new Error(`Unknown execution error: ${e}`)
+            },
+            handler: async function (code, id) {
+                if (!(await isBuildTimeFile(id, code))) return
+
+                const start = Date.now()
+                logger.info("\n")
+                logger.info(`Running build time script: ${id}`, { timestamp: true })
+
+                const ssrEnv = buildServer.environments.ssr
+                const runner = (ssrEnv as any)["runner"] as ModuleRunner
+
+                try {
+                    const modules = await runner.import(id)
+                    logger.info(`Done in ${Date.now() - start}ms`, { timestamp: true })
+
+                    const currentModule = buildServer.moduleGraph.getModuleById(id)
+
+                    Array.from(currentModule?.importedModules ?? []).forEach(m => {
+                        if (m.id) {
+                            this.addWatchFile(m.id)
+                        }
+                    })
+
+                    return serializeModules(modules, id)
+                } catch (e) {
+                    if (e instanceof Error) {
+                        throw new Error(`Failed to run build time script: ${id} \n${e.message}\n${e.stack}`)
+                    }
+                    throw new Error(`Unknown execution error: ${e}`)
+                }
             }
         },
         async buildEnd() {
-            console.log("build end")
             await buildServer.close()
         }
     }
